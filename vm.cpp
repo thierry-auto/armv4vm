ab75d5a5d739dfe7cd3b23defeb5c32c319a4f59
thierry-auto <thierry.auto@mailbox.org>
2020-03-16 11:54:52 +0100

    Set licence info

--- vm.cpp
+++ vm.cpp
@@ -1,3 +1,20 @@
+//    Copyright (c) 2020, thierry vic
+
+//    This file is part of armv4vm.
+
+//    armv4vm is free software: you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation, either version 3 of the License, or
+//    (at your option) any later version.
+
+//    armv4vm is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU General Public License for more details.
+
+//    You should have received a copy of the GNU General Public License
+//    along with armv4vm.  If not, see <http://www.gnu.org/licenses/>.
+
 #include "vm.h"
 
 #ifdef QT_CORE_LIB

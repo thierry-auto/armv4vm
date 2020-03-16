a034dc913be01946b737b55878bd6421d9c5d122
thierry-auto <thierry.auto@mailbox.org>
2020-03-16 11:54:52 +0100

    Set licence info

--- test.cpp
+++ test.cpp
@@ -1,3 +1,20 @@
+//    Copyright (c) 2020, thierry vic
+
+//    This file is part of armv4vm.
+
+//    armv4vm is free software: you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation, either version 3 of the License, or
+//    (at your option) any later version.
+
+//    armv4vm is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU General Public License for more details.
+
+//    You should have received a copy of the GNU General Public License
+//    along with armv4vm.  If not, see <http://www.gnu.org/licenses/>.
+
 #include <QObject>
 #include <QtTest>
 
@@ -1267,8 +1284,6 @@ void TestVm::testSTR2() {
     QVERIFY(vm.m_registers[0] == 0x11223344);
     QVERIFY(vm.m_registers[1] == 0x00000084);
 }
-
-
 
 
 
